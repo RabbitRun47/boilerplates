b167cd999316529c15e337d948fbff7d70b703c6
RabbitRun47 <mayankcf@gmail.com>
2022-10-24 13:47:12 -0400

    feat: RNJs

--- ReactNative/RNJavaScript/src/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import {sampleCounterSlice} from './sampleCounter';
-
-export const rootReducer = configureStore({
-  reducer: {
-    sampleCounter: sampleCounterSlice.reducer,
-  },
-});
--- ReactNative/RNJavaScript/src/screens/index.js
+++
@@ -1,5 +0,0 @@
-import {Home} from './Home';
-
-export const SCREENS = {
-  HOME: {name: 'Home', component: Home, options: {title: 'Home'}},
-};
